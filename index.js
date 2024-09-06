2e8c64f8a218f99a4f9c26555c01f47ced4c40e2
Kritik Aggarwal <98749974+kritik05@users.noreply.github.com>
2024-09-06 21:25:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const cors = require("cors");
 const rootRouter = require("./routes/index");
 
 const app = express();
-
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:5173'], // add your frontend's actual deployed URL here
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // specify allowed methods
+  credentials: true, // include credentials (cookies, etc.)
+  optionsSuccessStatus: 200 // some legacy browsers choke on 204
+};
+// app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/k", rootRouter);
